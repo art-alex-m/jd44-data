1670f6a09ae22b8eee6c22c7851c317e7aa0d3ff
Alexander M Artamonov <art.alex.m@gmail.com>
2023-12-18 13:03:42 +0300

    8.3. JDBC API, JdbcTemplate, NamedParametersJdbcTemplate. Initial commit

---
+++ l31_springjbdc/src/main/java/ru/netology/data/l31_springjbdc/L31SpringjbdcApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.data.l31_springjbdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L31SpringjbdcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L31SpringjbdcApplication.class, args);
+    }
+
+}
